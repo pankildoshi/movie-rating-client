3a7c5374552c77677a8361233a7f38d49887740f
pankildoshi <pankil19doshi@gmail.com>
2022-11-09 12:03:16 +0530

    Invalid Import Removed

--- src/components/MovieDetail.js
+++ src/components/MovieDetail.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Loader from "./Loader";
-import YoutubeEmbed from "./YoutubeEmbed";
 
 export default function MovieDetail() {
   const [movie, setMovie] = useState(null);
